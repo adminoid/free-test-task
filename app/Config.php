2030f8a46f02876e57852962733689204fef6c15
Petr Zenin <mr@adminoid.com>
2019-01-15 15:39:20 +0300

    assets stuff completed

--- app/Config.php
+++ app/Config.php
@@ -4,8 +4,8 @@ namespace App;
 
 class Config {
     /**
-     * path to the sqlite file (from project root folder)
+     * path to the sqlite file (from project public folder)
      */
-    const PATH_TO_SQLITE_FILE = 'db/db.sqlite';
+    const PATH_TO_SQLITE_FILE = '../db/db.sqlite';
 
 }
